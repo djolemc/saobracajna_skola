732f81957a7af85382175624d2163b3a313cfa3c
Korisnik <Korisnik@DESKTOP-C6DMF54>
2018-05-15 12:13:05 +0200

    Poruka

--- application/views/admin/administracija.php
+++ application/views/admin/administracija.php
@@ -6,7 +6,7 @@
 
     <div class="col-md-6 ">
 
-        <form action="/action_page.php">
+        <form name="administracija" action="<?php echo site_url("admin/unos_operatera") ?>" method="POST">
             <fieldset>
                 <legend>Unos novih operatera:</legend>
                 Ime:<br>
@@ -14,18 +14,21 @@
                 Prezime:<br>
                 <input type="text" name="prezime" placeholder="Unesite prezime"><br>
                 Korisnicko ime:<br>
-                <input type="text" name="lastname" placeholder="Kor. ime:"><br>
+                <input type="text" name="kor_ime" placeholder="Kor. ime:"><br>
                  Lozinka:<br>
-                <input type="password" name="lastname" placeholder="Kor. ime:"><br>
+                <input type="password" name="lozinka" placeholder="Kor. ime:"><br>
                 Ponovite lozinku:<br>
-                <input type="password" name="lastname" placeholder="Lozinka:"><br>
+                <input type="password" name="lozinka2" placeholder="Lozinka:"><br>
                 E-mail:<br>
-                <input type="email" name="lastname" placeholder="Lozinka:"><br><br>
+                <input type="email" name="email" placeholder="Lozinka:"><br><br>
                 <input type="hidden" name="tip" value="1">
                 <input type="submit" value="Submit"> <br>
             </fieldset>
         </form>
-
+        <?php
+                    $poruka = $this->session->userdata('poruka');
+                  echo  $poruka;
+                    ?>
 
     </div>
 
